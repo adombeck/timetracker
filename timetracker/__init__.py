635b0715b096acc32c1c29644f27a1800753c296
Adrian Dombeck <adrian.dombeck@rub.de>
2018-10-13 22:29:13 +0200

    Add menu to status icon

--- timetracker/__init__.py
+++ timetracker/__init__.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
 DATA_DIR = Path(Path.home(), ".local/share/timetracker")
-TASK_DIR = Path(DATA_DIR, "tasks")
+TASK_DIR = Path(DATA_DIR, "tasks")  # type: Path
 UI_DIR = Path("/usr/local/share/timetracker/ui")
-NEW_TASK_DIALOG_PATH = Path(UI_DIR, "new-task-dialog.ui")
+NEW_TASK_DIALOG_UI_PATH = Path(UI_DIR, "new-task-dialog.ui")
