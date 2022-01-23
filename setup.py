003ae00ea7e219f3e7ee64145b680a161d4355cc
Adrian Dombeck <adrian.dombeck@rub.de>
2022-01-23 19:03:20 +0100

    setup.py: Print command to set GNOME keybindings

--- setup.py
+++ setup.py
@@ -1,10 +1,31 @@
 #!/usr/bin/env python3
+import sys
 
-from distutils.core import setup
+from setuptools import setup
+from setuptools.command.install import install
 import subprocess
 import glob
 
+
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+
+    def run(self):
+        install.run(self)
+        subprocess.check_call(["update-desktop-database", "/usr/local/share/applications"])
+
+        print('''
+To add GNOME keybindings (given that set-gnome-keybinding.py is installed), run:
+
+set-gnome-keybinding.py "Start clocking new task" "timetracker --new" "F7"
+set-gnome-keybinding.py "Toggle clocking" "timetracker --toggle" "F8"
+''', file=sys.stderr)
+
+
 setup(
+    cmdclass={
+        'install': PostInstallCommand,
+    },
     name='timetracker',
     version='0.1',
     description='Track time spent on tasks',
@@ -18,5 +39,3 @@ setup(
     ],
     requires=['gi'],
 )
-
-subprocess.check_call(["update-desktop-database", "/usr/local/share/applications"])
