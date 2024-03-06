45044859a2ffff756edffbe98a0ad78b1820cc27
Adrian Dombeck <adrian.dombeck@gmail.com>
2024-03-06 15:03:37 +0100

    Fix f-string without any placeholders

--- timetracker/util.py
+++ timetracker/util.py
@@ -6,8 +6,9 @@ def seconds_to_timestr(seconds: int, show_seconds=False):
     minutes, seconds = divmod(seconds, 60)
     hours, minutes = divmod(minutes, 60)
     if show_seconds:
-        return f"%i:%.2i:%.2i" % (hours, minutes, seconds)
+        return "%i:%.2i:%.2i" % (hours, minutes, seconds)
     return "%i:%.2i" % (hours, minutes)
+
 
 def daterange(start_date, end_date) -> Iterable[datetime]:
     for n in range(int((end_date - start_date).days)+1):
